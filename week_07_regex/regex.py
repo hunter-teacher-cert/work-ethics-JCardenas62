60beb29e0e163a50814909955f0f29df2bb4abce
Jovani Cardenas <jac897@gmail.com>
2021-11-03 17:18:52 -0400

    added regex

---
+++ week_07_regex/regex.py
@@ -0,0 +1,19 @@
+#got it searching for all names with a title in front.
+#cannot get it to search for names without a title. 
+
+import re
+
+def find_name(line):
+    pattern = r"(Mr|Mrs|Ms)\.?( [A-Z][a-z]*)( [A-Z][A-Za-z]*)*"
+    result = re.findall(pattern,line)
+
+    pattern = r"John Smith"
+    result = result + re.findall(pattern,line)
+    return result
+
+f = open("namefile.dat")
+for line in f.readlines():
+    #print(line)
+    result = find_name(line)
+    if (len(result)>0):
+        print(result)
