502b0ae2197a9dd173414a161a92c01127487749
Jovani  Cardenas <86058181+JCardenas62@users.noreply.github.com>
2021-12-15 23:15:13 -0500

    Create functions.py

---
+++ week_01_python/functions.py
@@ -0,0 +1,27 @@
+def addab(a,b):
+    c = a + b
+    return c
+
+def subab(a,b):
+    d = a - b
+    return c
+
+ def multab(a,b):
+  e = a * b
+  return c
+def divab(a,b)
+  f = a / b
+  return c
+
+def main():
+    c = addab(50,30)
+    d = subab(50,30)
+    e = multab(50,30)
+    f = divab(50,30)
+    
+    print(c)
+    print(d)
+    print(e)
+    print(f)
+
+main()
